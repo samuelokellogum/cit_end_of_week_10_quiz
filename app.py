8c403242b2b6a4745eeb16123bd5b993b81816f0
Samuel Okellogum <oksam21@gmail.com>
2022-10-23 19:50:38 +0300

    commit_2

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask
-from urllib.request import urlopen
 import requests
+from bs4 import BeautifulSoup
+from flask import Flask
 
 app = Flask(__name__)
 url = "https://www.cbsnews.com/latest/rss/main"
 page = requests.get(url)
 
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
-
-print(html)
+print(page)
