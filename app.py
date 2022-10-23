a80d32900d22f4c79889f8bf2b88e624067af60d
Samuel Okellogum <oksam21@gmail.com>
2022-10-23 21:22:24 +0300

    commit_5

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask
+import os
+from lxml import etree
+import json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-url = "https://www.cbsnews.com/latest/rss/main"
-page = requests.get(url)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-print(page)
+class CbsNews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    link = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=True, nullable=False)
+ 
+
+    @app.route('/')
+    def index():
+        url = "https://www.cbsnews.com/latest/rss/main"
+        page = requests.get(url)
+        data = page.json()
+        # html_path = os.path.join(os.path.dirname(__file__), url) 
+        # root = etree.fromstring(url)
+        
+        return page
+        # result = etree.tostring(root)   
+        # with open(result, 'r') as html_file:
+        #    doc = html_file.read()
+
+        # soup = BeautifulSoup(doc, 'html.parser')
+        # return soup
+
+
+    @app.route('/cbs_news')
+    def cbs_news():
+        pass
+
+if __name__ == "__main__":
+    index()
+    app.run(debug =True)
