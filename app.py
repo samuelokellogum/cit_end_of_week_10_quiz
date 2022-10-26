28c9f5a7f0a7125a39a644a1e5a16f063822049b
Samuel Okellogum <oksam21@gmail.com>
2022-10-26 16:29:35 +0300

    commit_12

--- app.py
+++ app.py
@@ -1,33 +1,53 @@
-import os
-import json
+from flask import Blueprint
+from fruits.models import CBSNews
 import requests
-from lxml import etree
-from flask import Flask, render_template
-from datetime import datetime
 from bs4 import BeautifulSoup
-from models import model
 
-app = Flask(__name__)
 
-@app.route('/cbs_news', methods=["GET"])
-def cbs_news():
-    url = "https://www.cbsnews.com/latest/rss/main"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, features="xml")
+cviews = Blueprint('cbs', __name__, url_prefix='/cbs_news')
+
+# /cbs_news
+# /cbs_news/add
+
+def scrape_cbs():
+    url = 'https://www.cbsnews.com/latest/rss/main'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, features="xml")
+    news = soup.find_all('item')
+    data = []
+    for new in news:
+        # print(new.link.text)
+        # <Link />
+        title = new.title.text
+        link = new.link.text
+        image = new.image.text
+        description = new.description.text
+        data.append({
+            'title': title,
+            'link': link,
+            'image': image,
+            'description': description,
+        })
+    return data
+       
+@cviews.route('/')
+def index():
+    data = scrape_cbs()
     
-    # return soup.find_all('item')
-    my_list = {}
-    for data in soup:
-        val = {
-            "title": data.title.text,
-            "link": data.link.text,
-            "description": data.description.text,
-            "image": data.image.text,
-        }
-        
-        my_list['data'] = val 
-    return my_list
+    # existing data
+    cbs_data = CBSNews.get_all_news()
 
-if __name__ == "__main__":
-    index()
-    app.run(debug =True)
+    # check if data already exists
+    for news in data:
+        if news['title'].lower() not in [cbs.title.lower() for cbs in cbs_data]:
+            cbs = CBSNews(
+                title=news['title'],
+                link=news['link'],
+                image=news['image'],
+                description=news['description'],
+            )
+            cbs.save()
+        else:
+            continue
+
+    return {'data': [cbs.serialize() for cbs in CBSNews.get_all_news()]}
